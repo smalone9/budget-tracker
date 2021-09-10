3e465f44eda31c3cc1f7f9d75d6cc5ab17d018e1
Samantha Malone <sammac99@gmail.com>
2021-09-10 13:05:33 -0700

    finished idb

--- public/js/idb.js
+++ public/js/idb.js
@@ -19,7 +19,7 @@ request.onsuccess = function (event) {
   // check if app is online, if yes run uploadTotal() function to send all local db data to api
   if (navigator.onLine) {
     // we haven't created this yet, but we will soon, so let's comment it out for now
-    // uploadTotal();
+    uploadTotal();
   }
 };
 
@@ -27,3 +27,61 @@ request.onerror = function (event) {
   // log error here
   console.log(event.target.errorCode);
 };
+
+// This function will be executed if we attempt to submit a new total and there's no internet connection
+function saveRecord(record) {
+  // open a new transaction with the database with read and write permissions
+  const transaction = db.transaction(["new_total"], "readwrite");
+
+  // access the object store for `new_total`
+  const totalObjectStore = transaction.objectStore("new_total");
+
+  // add record to your store with add method
+  totalObjectStore.add(record);
+}
+
+function uploadTotal() {
+  // open a transaction on your db
+  const transaction = db.transaction(["new_total"], "readwrite");
+
+  // access your object store
+  const totalObjectStore = transaction.objectStore("new_total");
+
+  // get all records from store and set to a variable
+  const getAll = totalObjectStore.getAll();
+
+  // upon a successful .getAll() execution, run this function
+  getAll.onsuccess = function () {
+    // if there was data in indexedDb's store, let's send it to the api server
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((serverResponse) => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          // open one more transaction
+          const transaction = db.transaction(["new_total"], "readwrite");
+          // access the new_total object store
+          const totalObjectStore = transaction.objectStore("new_total");
+          // clear all items in your store
+          totalObjectStore.clear();
+
+          alert("All saved totals have been submitted!");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+}
+
+// listen for app coming back online
+window.addEventListener("online", uploadTotal);
